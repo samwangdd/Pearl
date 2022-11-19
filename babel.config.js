b19d29943336eaf236c5758cfb409d306be4e6f0
samwangdd <wyysammore@gmail.com>
2022-11-19 23:21:57 +0800

    chore: set alias

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ["module-resolver", {
+        "root": ["./"],
+        "extensions": [".ts", ".tsx"],
+        "alias": {
+          "@": "./src",
+          "@/components": "./src/components",
+          "@/pages": "./src/pages",
+          "@/const": "./src/const",
+          "@/utils": "./src/utils",
+          "@/service": "./src/service",
+          "@/types": "./src/types",
+          "@/hooks": "./src/hooks"
+          }
+        }],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
