b2d759afd5087aa38ac7fa7b45578951349c7053
samwangdd <wyysammore@gmail.com>
2022-11-20 07:52:41 +0800

    chore: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['@react-native-community', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
   rules: {
-  }
-}
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+        printWidth: 120,
+      },
+    ],
+    semi: 'off',
+    'react-native/no-inline-styles': 'off',
+    'no-shadow': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'no-bitwise': 0,
+  },
+  globals: {
+    AbortController: true,
+    Response: true,
+    RequestInit: true,
+  },
+};
