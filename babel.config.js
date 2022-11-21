195bc06c2d20c136bb59216d5bc910591a237c66
samwangdd <wyysammore@gmail.com>
2022-11-21 23:31:21 +0800

    chore: babel

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ["module-resolver", {
-        "root": ["./"],
-        "extensions": [".ts", ".tsx"],
-        "alias": {
-          "@": "./src",
-          "@/components": "./src/components",
-          "@/pages": "./src/pages",
-          "@/const": "./src/const",
-          "@/utils": "./src/utils",
-          "@/service": "./src/service",
-          "@/types": "./src/types",
-          "@/hooks": "./src/hooks"
-          }
-        }],
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.ts', '.tsx'],
+          alias: {
+            '@': './src',
+            '@/components': './src/components',
+            '@/pages': './src/pages',
+            '@/const': './src/const',
+            '@/utils': './src/utils',
+            '@/service': './src/service',
+            '@/types': './src/types',
+            '@/hooks': './src/hooks',
+          },
+        },
+      ],
       'react-native-reanimated/plugin',
     ],
   };
